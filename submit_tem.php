99e8bfac4c6746bacd19b18063ec3f079e324cc7
praise <qiqiyoujian@gmail.com>
2013-05-20 10:50:30 +0800

    添加编码

--- submit_tem.php
+++ submit_tem.php
@@ -105,6 +105,7 @@ function getCategories(){
 <html>
 <head>
     <title>提交模版-ACT-DPL</title>
+    <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
